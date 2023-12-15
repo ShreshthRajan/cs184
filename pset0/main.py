f6c4614fee40be9565a16391056e1bc752b1c5e7
SHRESHTH RAJAN <shreshthrajan@college.harvard.edu>
2023-12-15 12:30:17 -0500

    done

--- pset0/main.py
+++ pset0/main.py
@@ -1,23 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Set a sufficiently large n
+# Improve plot aesthetics with Seaborn
+sns.set()
+
+# Set a sufficiently large n (assumed to be 10000 here)
 n = 10000
 
-# Generate n samples from a standard normal distribution
+# Generate n samples from a standard normal distribution (mean 0, variance 1)
 Z = np.random.randn(n)
 
-# Calculate the empirical CDF
+# Calculate the empirical CDF of Z
 sorted_Z = np.sort(Z)
-empirical_cdf = np.arange(1, n + 1) / float(n)
+empirical_cdf_Z = np.arange(1, n + 1) / float(n)
 
-# Plot the empirical CDF
-plt.step(sorted_Z, empirical_cdf)
-plt.title('Empirical CDF of Standard Normal Distribution')
+# Define k values
+k_values = [1, 8, 64, 512]
+
+# Plot the empirical CDF for each k
+plt.figure(figsize=(10, 6))
+plt.step(sorted_Z, empirical_cdf_Z, label='Standard Normal')
+
+for k in k_values:
+    Y_k = np.sum(np.sign(np.random.randn(n, k)) * np.sqrt(1. / k), axis=1)
+    sorted_Y_k = np.sort(Y_k)
+    empirical_cdf_Y_k = np.arange(1, n + 1) / float(n)
+    plt.step(sorted_Y_k, empirical_cdf_Y_k, label=f'Y({k})')
+
+plt.title('Empirical CDFs of Standard Normal and Y(k)')
 plt.xlabel('x')
 plt.ylabel('Empirical CDF')
+plt.legend()
 plt.xlim(-3, 3)
+
+# Save the plot as a PNG file in the current directory
+plt.savefig("empirical_cdf_combined.png")
+
+# Display the plot
 plt.show()
+
+
+# part A
+# # Set a sufficiently large n (assumed to be 10000 here)
+# # You should verify this n based on the condition from problem 3.5 if applicable
+# n = 10000
+
+# # Generate n samples from a standard normal distribution (mean 0, variance 1)
+# # This simulates the random variables Z_i ~ N(0, 1) for i = 1, ..., n
+# Z = np.random.randn(n)
+
+# # Calculate the empirical CDF of the generated samples
+# # The empirical CDF at a point x is the proportion of samples less than or equal to x
+# sorted_Z = np.sort(Z)
+# empirical_cdf = np.arange(1, n + 1) / float(n)
+
+# # Plot the empirical CDF
+# # The step plot represents the cumulative distribution function of the generated samples
+# plt.step(sorted_Z, empirical_cdf)
+# plt.title('Empirical CDF of Standard Normal Distribution')
+# plt.xlabel('x')
+# plt.ylabel('Empirical CDF')
+# plt.xlim(-3, 3) # Limiting the x-axis to [-3, 3] to focus on the central part of the distribution
+
+# # Save the plot as a PNG file in the current directory
+# plt.savefig("empirical_cdf.png")
+
+# # Display the plot
+# plt.show()
 
 
 # Question 6) 1. 
